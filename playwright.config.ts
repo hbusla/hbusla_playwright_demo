8953a2ad26059cb1fa815b45f466881ecf9c2528
hbusla <hanna.busla@airslate.com>
2024-09-11 18:50:08 +0400

    Update json report

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html'], 
+    ['html', { outputFolder: 'playwright-report' }],
     ['json', { outputFile: 'playwright-report/test_result_json.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
